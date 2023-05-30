77a416b663d4d4e1686b503eaf829650a1e01b4d
Alptekin <alptekin.ocakdan@gmail.com>
2023-05-30 20:29:15 +0300

    Google audio/video/docs direct download and embed link creator

--- 08GoogleDriveApp/script.js
+++ 08GoogleDriveApp/script.js
@@ -1,10 +1,9 @@
-const gLink= document.getElementById("glink");
+const gLink = document.getElementById("glink");
 const btn = document.getElementById("btn");
 const downloadLink = document.getElementById("download-link");
 const embedAudio = document.getElementById("embed-audio");
 const embedVideo = document.getElementById("embed-video");
 const clear = document.querySelector(".clear");
-
 
 
 btn.addEventListener("click", generateLink);
@@ -17,20 +16,21 @@ function generateLink(e) {
 
     if (confirmLink == true) {
         const getDownloadLink = gLink.value
-        .replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id=")
-        .replace("/view?usp=sharing", "");
+            .replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id=")
+            .replace("/view?usp=sharing", "");
         // alert(getDownloadLink);
         downloadLink.value = getDownloadLink;
 
         function copyText(target) {
             if (target.value == "") {
                 alert("Please generate a Download Link")
-                }else {
+            } else {
                 target.select();
                 document.execCommand("copy");
                 alert("Link has been copied to clipboard");
-                }
+            }
         }
+
         const copy = document.querySelector(".copy");
         copy.addEventListener("click", () => {
             return copyText(downloadLink);
@@ -39,7 +39,7 @@ function generateLink(e) {
         // EMBED AUDIO
         const audio1 = '<audio width="300" height="32" controls="controls" src="';
         const audio2 = '" type="audio/mp3"></audio>';
-        
+
         console.log(downloadLink.value);
         embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
         // COPY AUDIO EMBED CODE
@@ -51,11 +51,11 @@ function generateLink(e) {
 
         // Embed Video
         const getVideoLink = gLink.value
-        .replace("/view?usp=sharing", "");
+            .replace("/view?usp=sharing", "");
 
         const video1 = '<iframe src="';
         const video2 = '/preview" width="560" height="315"></iframe>';
-        
+
         embedVideo.value = `${video1}${getVideoLink}${video2}`;
 
         const copyVideo = document.querySelector(".copy-video");
