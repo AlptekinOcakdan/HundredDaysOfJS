690710d4d74cc99774c6a83d1c85fb8fd2b25c3d
Alptekin <alptekin.ocakdan@gmail.com>
2023-05-30 20:18:26 +0300

    Changing the deprecated function execCommand with navigator.clipboard.writeText().

--- 07ClickToCopy/script.js
+++ 07ClickToCopy/script.js
@@ -2,16 +2,21 @@ const btn = document.querySelector(".btn");
 const coupon = document.querySelector(".coupon");
 
 const copyText = (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  coupon.select();
-  coupon.setSelectionRange(0, 999999);
-  document.execCommand("copy");
-
-  btn.textContent = "Copied!!!";
-  setTimeout(() => {
-    btn.textContent = "Copy";
-  }, 3000);
+    /*
+        coupon.select();
+        coupon.setSelectionRange(0, 999999);
+        document.execCommand("copy");
+    */
+// execCommand is deprecated. Instead of that we use the navigator.
+// navigator.clipboard.writeText() return a Promise. So we use .then() to do what we want to do before.
+    navigator.clipboard.writeText(coupon.value).then(() => {
+        btn.textContent = "Copied!!!";
+        setTimeout(() => {
+            btn.textContent = "Copy";
+        }, 3000);
+    })
 };
 
 btn.addEventListener("click", copyText);
