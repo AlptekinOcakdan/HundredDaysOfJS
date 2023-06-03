f45c12d1ed4f16948c1d7d64e8c9cc5f18cb82b5
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-03 17:18:19 +0300

    Get Location App

--- 12API/script.js
+++ 12API/script.js
@@ -1,13 +1,14 @@
 const userLocation = document.querySelector(".location");
-const btn = document.querySelector(".btn");
+const btn=document.querySelector(".btn");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click",()=>{
   navigator.geolocation.getCurrentPosition(showPosition);
-});
+})
 
 function showPosition(position) {
-  userLocation.innerHTML = `
-        Latitude: ${position.coords.latitude} <br>
-        Longitude: ${position.coords.longitude}
-    `;
+  userLocation.innerHTML=`
+       Latitude: ${position.coords.latitude}<br>
+       Longitude: ${position.coords.longitude}
+       
+  `;
 }
