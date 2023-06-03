431b33e300fcd06cd462d163b2bd74aaca906b07
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-03 20:33:14 +0300

    Get Random Quotes App

--- 14AjaxQuotesGenerator/fetch.js
+++ 14AjaxQuotesGenerator/fetch.js
@@ -6,48 +6,42 @@ const URL = "https://type.fit/api/quotes";
 function getQuotes(e) {
     e.preventDefault();
 
-    if (number.value.length == 0) {
-        return alert("Plese enter a number");
-    } else {
-        fetch(URL)
-        .then(function(response) {
+    if (number.value.length === 0) {
+        alert("Please enter a number");
+        return;
+    }
+
+    fetch(URL)
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
-            // console.log(JSON.stringify(data));
+        .then(function (data) {
             data = shuffle(data);
-            
             let output = "";
-
-            
-            for (let i = 0; i < data.length; i++) {
-                if (i == number.value) {break;}
+            const limit = parseInt(number.value, 10);
+            for (let i = 0; i < limit && i < data.length; i++) {
                 output += `
                     <li>Quote: ${data[i].text}</li>
                     <li>Author: ${data[i].author}</li>
-                   <hr>
-               `;
+                    <hr>
+                `;
             }
             document.querySelector(".quotes").innerHTML = output;
-        })
-    }
+        });
 }
 
 
-//  FUNCTION TO SHUFFLE QUOTES
-
 function shuffle(quotes) {
-    let CI = quotes.length, tempValue, randomIndex;
+    let CI = quotes.length;
+    let tempValue, randomIndex;
 
-    // While elements exist in the array
-    while (CI > 0) {
+    while (CI >= 1) {
         randomIndex = Math.floor(Math.random() * CI);
-        // DECREASE CI BY 1
         CI--;
-        // SWAP THE LAST ELEMENT WITH CI
         tempValue = quotes[CI];
         quotes[CI] = quotes[randomIndex];
         quotes[randomIndex] = tempValue;
     }
+
     return quotes;
 }
