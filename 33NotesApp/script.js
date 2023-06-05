ec1f81894366e0e815deddbec153fc7b1054b835
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 15:11:14 +0300

    Notes App with course code.

--- 33NotesApp/script.js
+++ 33NotesApp/script.js
@@ -1,45 +1,43 @@
 const noteBtn = document.getElementById("add-btn"),
-  noteTitle = document.getElementById("note-title"),
-  noteText = document.getElementById("note-text"),
-  clear = document.querySelector(".clear");
+    noteTitle = document.getElementById("note-title"),
+    noteText = document.getElementById("note-text"),
+    clear = document.querySelector(".clear");
+let notesObj;
 
-//   Get notes from local storage
 function getNotes() {
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
 }
 
-//   Note btn event listener
 noteBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  if (noteTitle.value == "" || noteText.value == "") {
-    return alert("please add note title and details");
-  }
+    if (noteTitle.value == "" || noteText.value == "") {
+        return alert("please add note title and details");
+    }
 
-  getNotes(); // notesObj array
+    getNotes(); // notesObj array
 
-  let myObj = {
-    title: noteTitle.value,
-    text: noteText.value,
-  };
-  notesObj.push(myObj);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
+    let myObj = {
+        title: noteTitle.value,
+        text: noteText.value,
+    };
+    notesObj.push(myObj);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
 
-  document.querySelector("form").reset();
-  showNotes();
+    document.querySelector("form").reset();
+    showNotes();
 });
 
-// Display notes on the page
 function showNotes() {
-  getNotes();
-  let html = "";
-  notesObj.forEach(function (element, index) {
-    html += `
+    getNotes();
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
         <div class="note">
         <div class="note-cta">
           <p class="note-counter">Note ${index + 1}</p>
@@ -57,46 +55,43 @@ function showNotes() {
         <p class="note-text">${element.text}</p>
       </div>
         `;
-  });
-  let noteElm = document.getElementById("notes");
+    });
+    let noteElm = document.getElementById("notes");
 
-  if (notesObj.length != 0) {
-    noteElm.innerHTML = html;
-  } else {
-    noteElm.innerHTML = "No notes added, Please add a note";
-  }
+    if (notesObj.length !== 0) {
+        noteElm.innerHTML = html;
+    } else {
+        noteElm.innerHTML = "No notes added, Please add a note";
+    }
 }
 
-// DELETE A SINGLE NOTE
 function deleteNote(index) {
-  let confirmDel = confirm("Delete this note");
-  if (confirmDel) {
+    let confirmDel = confirm("Delete this note");
+    if (confirmDel) {
+        getNotes();
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
+}
+
+clear.addEventListener("click", () => {
+    localStorage.clear();
+    showNotes();
+});
+
+function editNote(index) {
+    if (noteTitle.value !== "" || noteText.value !== "") {
+        return alert("Please clear the form before editing");
+    }
     getNotes();
+
+    noteTitle.value = notesObj[index].title;
+    noteText.value = notesObj[index].text;
+
     notesObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
-  }
-}
-
-// Delete all notes
-clear.addEventListener("click", () => {
-  localStorage.clear();
-  showNotes();
-});
-
-// Edit note
-function editNote(index) {
-  if (noteTitle.value !== "" || noteText.value !== "") {
-    return alert("Please clear the form before editing");
-  }
-  getNotes();
-
-  noteTitle.value = notesObj[index].title;
-  noteText.value = notesObj[index].text;
-
-  notesObj.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
 }
 
 showNotes();
