b5c17da882e2cf32e2d0b9229d97fcbfc3f7e112
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 13:04:20 +0300

    Responsive Menu App

--- 24ResponsiveMobileMenu/script.js
+++ 24ResponsiveMobileMenu/script.js
@@ -1,34 +1,47 @@
 const links = document.querySelectorAll(".nav-list li a");
-
-for (link of links) {
-  link.addEventListener("click", smoothScroll);
-}
-
-function smoothScroll(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  document.querySelector(href).scrollIntoView({
-    behavior: "smooth",
-  });
-  hideMenu();
-}
-
-// RESPONSIVE MOBILE MENU
 const menu = document.querySelector(".nav-list");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 
+for (let link of links) {
+    link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+    });
+    hideMenu();
+}
+
+
 const showMenu = () => {
-  hamburger.style.display = "none";
-  close.style.transform = "translateY(0)";
-  menu.style.transform = "translateY(0)";
+    hamburger.style.display = "none";
+    close.style.transform = "translateY(0)";
+    menu.style.transform = "translateY(0)";
 };
 
 const hideMenu = () => {
-  close.style.transform = "translateY(-20rem)";
-  hamburger.style.display = "block";
-  menu.style.transform = "translateY(-200%)";
+    close.style.transform = "translateY(-20rem)";
+    hamburger.style.display = "block";
+    menu.style.transform = "translateY(-200%)";
 };
 
 hamburger.addEventListener("click", showMenu);
 close.addEventListener("click", hideMenu);
+
+function handleResize() {
+    if (window.innerWidth >= 600) {
+        menu.style.transform = "translateY(0)";
+    } else {
+        menu.style.transform = "translateY(-200%)";
+    }
+}
+
+// Add event listener for window resize
+window.addEventListener("resize", handleResize);
+
+// Call handleResize initially to set the initial state based on window width
+handleResize();
