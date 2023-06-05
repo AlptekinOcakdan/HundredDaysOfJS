b0ad21eac00b0c7eb4041db96aaf182636d55d27
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 13:29:13 +0300

    Responsive Mobile Sub-Menu App

--- 26ResponsiveMobileMenuSubmenu/script.js
+++ 26ResponsiveMobileMenuSubmenu/script.js
@@ -1,16 +1,16 @@
 const links = document.querySelectorAll(".nav-list li a");
 
-for (link of links) {
-  link.addEventListener("click", smoothScroll);
+for (let link of links) {
+    link.addEventListener("click", smoothScroll);
 }
 
 function smoothScroll(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  document.querySelector(href).scrollIntoView({
-    behavior: "smooth",
-  });
-  hideMenu();
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+    });
+    hideMenu();
 }
 
 // RESPONSIVE MOBILE MENU
@@ -20,18 +20,18 @@ const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 
 const showMenu = () => {
-  hamburger.style.display = "none";
-  close.style.transform = "translateY(0)";
-  menuWrapper.style.transform = "translateX(0)";
-  menu.style.transform = "translateX(0)";
+    hamburger.style.display = "none";
+    close.style.transform = "translateY(0)";
+    menuWrapper.style.transform = "translateX(0)";
+    menu.style.transform = "translateX(0)";
 };
 
 const hideMenu = () => {
-  close.style.transform = "translateY(-20rem)";
-  hamburger.style.display = "block";
-  menuWrapper.style.transform = "translateX(-200%)";
-  menu.style.transform = "translateX(200%)";
-  subMenuThree.style.transform = "translateX(-100%)";
+    close.style.transform = "translateY(-20rem)";
+    hamburger.style.display = "block";
+    menuWrapper.style.transform = "translateX(-200%)";
+    menu.style.transform = "translateX(200%)";
+    subMenuTree.style.transform = "translateX(-100%)";
 };
 
 hamburger.addEventListener("click", showMenu);
@@ -41,14 +41,29 @@ menuWrapper.addEventListener("click", hideMenu);
 // Submenu Section
 const thirdLink = document.querySelector(".third-link");
 const back = document.querySelector(".back-to-menu");
-const subMenuThree = document.querySelector(".submenu-three");
+const subMenuTree = document.querySelector(".submenu-tree");
 
 thirdLink.addEventListener("click", () => {
-  menu.style.transform = "translateX(-100%)";
-  subMenuThree.style.transform = "translateX(0)";
+    menu.style.transform = "translateX(-100%)";
+    subMenuTree.style.transform = "translateX(0)";
 });
 
 back.addEventListener("click", () => {
-  menu.style.transform = "translateX(0)";
-  subMenuThree.style.transform = "translateX(-100%)";
+    menu.style.transform = "translateX(0)";
+    subMenuTree.style.transform = "translateX(-100%)";
 });
+
+
+function handleResize() {
+    if (window.innerWidth >= 600) {
+        menuWrapper.style.transform = "translateX(0)";
+        menu.style.transform = "translateX(0)";
+    } else {
+        menuWrapper.style.transform = "translateX(-200%)";
+        menu.style.transform = "translateX(200%)";
+    }
+}
+
+window.addEventListener("resize", handleResize);
+
+handleResize();
