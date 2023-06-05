3e935c2da6825ae363e1d16d6d4b5c525f6810cd
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 13:59:02 +0300

    Local Storage Usage

--- 31LocalStorage/script.js
+++ 31LocalStorage/script.js
@@ -1,22 +1,19 @@
-// console.log(window);
+/*
+LOCAL STORAGE METHODS
+setItem(): Add key and value to localStorage
+getItem(): This is how you get items from localStorage
+removeItem(): Remove an item by key from localStorage
+clear(): Clear all localStorage
+key(): Passed a number to retrieve the key of a localStorage
+*/
 
-// console.log(Object.getOwnPropertyNames(window));
-
-// LOCAL STORAGE METHODS
-// setItem(): Add key and value to localStorage
-// getItem(): This is how you get items from localStorage
-// removeItem(): Remove an item by key from localStorage
-// clear(): Clear all localStorage
-// key(): Passed a number to retrieve the key of a localStorage
-
-// window.localStorage.setItem("key", "value");
 
 window.localStorage.setItem("firstName", "Zino");
 localStorage.setItem("lastName", "Akpareva");
 
 const person = {
-  fullName: "Akpareva Zino",
-  location: "Abuja",
+    fullName: "Akpareva Zino",
+    location: "Abuja",
 };
 
 localStorage.setItem("user", JSON.stringify(person));
