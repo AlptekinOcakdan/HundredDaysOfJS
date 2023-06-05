0120bfae6e0984feb977931f08ef305a8ff19488
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 13:45:30 +0300

    Newsletter

--- 28NewletterFormOnScroll/script.js
+++ 28NewletterFormOnScroll/script.js
@@ -1,51 +1,59 @@
 const links = document.querySelectorAll(".nav-list li a");
 
-for (link of links) {
-  link.addEventListener("click", smoothScroll);
+for (let link of links) {
+    link.addEventListener("click", smoothScroll);
 }
 
 function smoothScroll(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  document.querySelector(href).scrollIntoView({
-    behavior: "smooth",
-  });
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+    });
 }
 
 // Sticky Header
 window.addEventListener("scroll", () => {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // Scroll Indicator JS
 window.onscroll = () => scrollProgress();
 
 function scrollProgress() {
-  const currentState =
-    document.body.scrollTop || document.documentElement.scrollTop;
+    const currentState =
+        document.body.scrollTop || document.documentElement.scrollTop;
 
-  const pageHeight =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
+    const pageHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
 
-  const scrollPercentage = (currentState / pageHeight) * 100;
+    const scrollPercentage = (currentState / pageHeight) * 100;
 
-  const progressBar = document.querySelector(".progress");
+    const progressBar = document.querySelector(".progress");
 
-  progressBar.style.visibility = "visible";
-  progressBar.style.width = scrollPercentage + "%";
-
-  // Newletter JS
-  const newsLetter = document.querySelector(".newsletter");
-
-  if (scrollPercentage > 80) {
-    newsLetter.style.transform = "translateX(0)";
-  } else {
-    newsLetter.style.transform = "translateX(-100%)";
-  }
-
-  document.querySelector(".fa-times").addEventListener("click", () => {
-    newsLetter.style.transform = "translateX(-100%)";
-  });
+    progressBar.style.visibility = "visible";
+    progressBar.style.width = scrollPercentage + "%";
 }
+
+function newsletter(){
+    const newsLetter = document.querySelector(".newsletter");
+
+    // Show the newsletter after 3 seconds
+    setTimeout(() => {
+        newsLetter.style.transform = "translateX(0)";
+    }, 3000);
+
+    // Hide the newsletter after 15 seconds
+    setTimeout(() => {
+        newsLetter.style.transform = "translateX(-100%)";
+    }, 15000);
+
+    // Close the newsletter manually when .fa-times is clicked
+    document.querySelector(".fa-times").addEventListener("click", () => {
+        newsLetter.style.transform = "translateX(-100%)";
+    });
+}
+
+newsletter();
