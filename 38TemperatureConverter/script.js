ad6e35e15c4e072de0f85c274c3bd56042b2429f
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 16:55:50 +0300

    Temperature Converter App

--- 38TemperatureConverter/script.js
+++ 38TemperatureConverter/script.js
@@ -1,29 +1,25 @@
-const celsius = document.querySelector(".celsius"),
-  fah = document.querySelector(".fahrenheit"),
-  kelvin = document.querySelector(".kelvin"),
-  form = document.querySelector("form");
+const celsius = document.querySelector(".celsius");
+const fah = document.querySelector(".fahrenheit");
+const kelvin = document.querySelector(".kelvin");
+const form = document.querySelector("form");
 
-form.addEventListener("input", convertWeight);
+form.addEventListener("input", convertTemperature);
 
-function convertWeight(e) {
+function convertTemperature(e) {
+  const value = e.target.valueAsNumber;
+
   if (e.target.classList.contains("celsius")) {
-    let x = e.target.value;
-    fah.value = x * 1.8 + 32;
-    kelvin.value = x + 273.15;
-  }
-
-  if (e.target.classList.contains("fahrenheit")) {
-    let x = e.target.value;
-    celsius.value = (x - 32) / 1.8;
-    kelvin.value = (x - 32) / 1.8 + 273.15;
-  }
-
-  if (e.target.classList.contains("kelvin")) {
-    let x = e.target.value;
-    fah.value = (x - 273.15) * 1.8 + 32;
-    celsius.value = parseFloat(x) - 273.15;
+    fah.value = (value * 1.8 + 32).toFixed(2);
+    kelvin.value = (value + 273.15).toFixed(2);
+  } else if (e.target.classList.contains("fahrenheit")) {
+    celsius.value = ((value - 32) / 1.8).toFixed(2);
+    kelvin.value = (((value - 32) / 1.8) + 273.15).toFixed(2);
+  } else if (e.target.classList.contains("kelvin")) {
+    fah.value = (((value - 273.15) * 1.8) + 32).toFixed(2);
+    celsius.value = (value - 273.15).toFixed(2);
   }
 }
+
 
 // --Convert Celsius to--
 // fah = (x * 1.8) + 32;
