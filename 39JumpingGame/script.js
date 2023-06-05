7c48409ec426fb585c8f817b43e93eb432d3d0eb
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-05 17:06:51 +0300

    Jumping game

--- 39JumpingGame/script.js
+++ 39JumpingGame/script.js
@@ -1,32 +1,28 @@
 const hero = document.querySelector(".hero");
-const heroBoy = document.querySelector(".hero-boy");
-const vilan = document.querySelector(".vilan");
+const villain = document.querySelector(".vilan");
 
 const jump = () => {
-  if (hero.classList != "animate") {
-    hero.classList.add("animate");
-    vilan.style.animation = "move 1s infinite linear";
-  }
-  setTimeout(() => {
-    hero.classList.remove("animate");
-  }, 300);
+    if (!hero.classList.contains("animate")) {
+        hero.classList.add("animate");
+        villain.style.animation = "move 1s infinite linear";
+    }
+    setTimeout(() => {
+        hero.classList.remove("animate");
+    }, 300);
 };
 
 document.addEventListener("keydown", (e) => {
-  if (e.code == "Space") {
-    jump();
-  }
+    if (e.code === "Space") {
+        jump();
+    }
 });
 
 let isAlive = setInterval(() => {
-  let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
-  //   console.log(heroTop);
-  let vilanLeft = parseInt(
-    window.getComputedStyle(vilan).getPropertyValue("left")
-  );
+    const heroTop = parseInt(window.getComputedStyle(hero).top);
+    const vilanLeft = parseInt(window.getComputedStyle(villain).left);
 
-  if (vilanLeft < 40 && vilanLeft > 20 && heroTop >= 130) {
-    vilan.style.animation = "none";
-    alert("Game over. Press spacebar to start");
-  }
+    if (vilanLeft < 40 && vilanLeft > 20 && heroTop >= 130) {
+        villain.style.animation = "none";
+        alert("Game over. Press spacebar to start");
+    }
 }, 10);
