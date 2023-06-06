8002429826fad515ca6ba142d0f7cb8132a5d028
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-06 12:34:55 +0300

    Github Profile Search App

--- 44GitHubProfileSearch/js/functions.js
+++ 44GitHubProfileSearch/js/functions.js
@@ -2,71 +2,82 @@ import * as v from "./variables.js";
 
 // Get Users
 export async function getUser(username) {
-  const response = await fetch(v.apiURL + username);
-  const data = await response.json();
+    const response = await fetch(v.apiURL + username);
+    const data = await response.json();
 
-  //   console.log(data);
-  if (!response.ok) {
-    errorMessage("User not found, try another");
-  } else {
-    displayData(data);
-    getRepos(username);
-  }
+    if (!response.ok) {
+        errorMessage("User not found, please try another username.");
+    } else {
+        displayData(data);
+        await getRepos(username);
+    }
 }
+
 
 // Get Repos
 async function getRepos(username) {
-  const response = await fetch(v.apiURL + username + "/repos");
-  const data = await response.json();
+    const response = await fetch(v.apiURL + username + "/repos");
+    const data = await response.json();
 
-  console.log(data);
-  displayRepos(data);
+    if (!response.ok) {
+        errorMessage("Failed to fetch repositories.");
+    } else {
+        displayRepos(data);
+    }
 }
 
 // Error message function
 export function errorMessage(msg) {
-  v.profile.innerHTML = "";
-  document.querySelector(".hide").style.display = "none";
-  return (v.repos.innerHTML = `<p class="alert alert-danger">${msg}</p>`);
+    clearProfileAndError();
+    v.repos.innerHTML = `<p class="alert alert-danger">${msg}</p>`;
 }
 
 function displayData(user) {
-  const html = `
+    const html = `
         <img
-        src="${user.avatar_url}"
-        alt="${user.name}"
-        class="img-thumbnail rounded-circle"
-    />
-    <h2>${user.name}</h2>
-    <p>${user.login}</p>
-    <div class="d-grid">
-        <a href="https://github.com/${user.login}" target="_blank" rel="noopener" class="btn btn-outline-secondary">View Profile</a>
-    </div>
-    <p class="pt-2">
-        <span>${user.followers} Followers</span>
-        <span>${user.following} Following</span>
-    </p>
-    <p>${user.public_repos}</p>
-    <p><i class="fas fa-marker-alt"></i>${user.location}</p>
+            src="${user.avatar_url}"
+            alt="${user.name}"
+            class="img-thumbnail rounded-circle"
+        />
+        <h2>${user.name}</h2>
+        <p>${user.login}</p>
+        <div class="d-grid">
+            <a href="https://github.com/${user.login}" target="_blank" rel="noopener" class="btn btn-outline-secondary">View Profile</a>
+        </div>
+        <p class="pt-2">
+            <span>${user.followers} Followers</span>
+            <span>${user.following} Following</span>
+        </p>
+        <p>${user.public_repos}</p>
+        <p><i class="fas fa-marker-alt"></i>${user.location}</p>
     `;
-  v.profile.innerHTML = html;
+    v.profile.innerHTML = html;
 }
 
-// Didplay Repos
+// Display Repos
 function displayRepos(repoData) {
-  let repo_data = repoData.map((repo) => {
-    return `
-        <span class="repo border border-rounded p-3">
-            <a href="${repo.html_url}" target="_blank" rel="noopener">${repo.name}</a>
-            <p>
-            <strong>Stars: ${repo.starazers_count} |</strong>
-            <strong>Watchers: ${repo.watchers_count} |</strong>
-            <strong>Forks: ${repo.forks_count}</strong>
-            </p>
-      </span>
-        `;
-  });
-  //   v.repos.innerHTML = repo_data;
-  v.repos.innerHTML = repo_data.slice(0, 8).join("");
-  document.querySelector(".hide").style.display = "block";
+    const repoHTML = repoData
+        .slice(0, 8)
+        .map((repo) => {
+            return `
+                <span class="repo border border-rounded p-3">
+                    <a href="${repo.html_url}" target="_blank" rel="noopener">${repo.name}</a>
+                    <p>
+                        <strong>Stars: ${repo.stargazers_count} |</strong>
+                        <strong>Watchers: ${repo.watchers_count} |</strong>
+                        <strong>Forks: ${repo.forks_count}</strong>
+                    </p>
+                </span>
+            `;
+        })
+        .join("");
+    v.repos.innerHTML = repoHTML;
+    v.hideElement.style.display = "block";
+}
+
+// Clear profile and error message
+function clearProfileAndError() {
+    v.profile.innerHTML = "";
+    v.repos.innerHTML = "";
+    v.hideElement.style.display = "none";
 }
--- 44GitHubProfileSearch/main.js
+++ 44GitHubProfileSearch/main.js
@@ -2,16 +2,18 @@ import * as v from "./js/variables.js";
 import { getUser, errorMessage } from "./js/functions.js";
 
 v.form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  //   let user = v.search.value;
-  //   let user = v.search.value.replace(/\s+/g, "");
-  let user = v.search.value.split(" ").join("");
+    e.preventDefault();
+    let user = v.search.value.split(" ").join("");
 
-  if (user === "") {
-    errorMessage("Input cannot be blank");
-    console.log("blank");
-  } else {
-    getUser(user);
-    v.search.value = "";
-  }
+    if (user === "") {
+        errorMessage("Input cannot be blank");
+    } else {
+        getUser(user)
+            .then(() => {
+                v.search.value = "";
+            })
+            .catch((error) => {
+                errorMessage(error.message);
+            });
+    }
 });
