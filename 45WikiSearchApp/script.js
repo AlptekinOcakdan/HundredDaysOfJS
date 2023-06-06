33f318be47420634c0afa0265c848c3072a3443d
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-06 12:59:11 +0300

    Search App for Wikipedia

--- 45WikiSearchApp/script.js
+++ 45WikiSearchApp/script.js
@@ -5,62 +5,67 @@ const errorMsg = document.querySelector(".alert");
 const line = document.querySelector("hr");
 
 const apiURL =
-  "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const searchValue = search.value;
-  if (searchValue === "") {
-    errorMessage("Search cannot be empty, please enter a search term.");
-  } else {
-    getResult(searchValue);
-  }
+    const searchValue = search.value;
+    if (searchValue === "") {
+        errorMessage("Search cannot be empty, please enter a search term.");
+    } else {
+        clearResults();
+        getResult(searchValue)
+            .then(() => {
+            })
+            .catch((error) => {
+                errorMessage(error.message);
+            });
+    }
 });
+
 
 // Error Message
 function errorMessage(msg) {
-  errorMsg.style.display = "block";
-  line.style.display = "block";
-  errorMsg.textContent = msg;
+    errorMsg.style.display = "block";
+    line.style.display = "block";
+    errorMsg.textContent = msg;
+    clearResults();
 }
 
 async function getResult(searchVal) {
-  const response = await fetch(apiURL + searchVal);
-  const results = await response.json();
+    const response = await fetch(apiURL + searchVal);
+    const results = await response.json();
 
-  console.log(results);
-  if (results.query.search.length == 0) {
-    return errorMessage("Invalid search, please enter another search term.");
-  } else {
-    displayResults(results);
-  }
+    console.log(results);
+    if (results.query.search.length === 0) {
+        return errorMessage("Invalid search, please enter another search term.");
+    } else {
+        displayResults(results);
+    }
 }
 
 // Display Results
 function displayResults(results) {
-  line.style.display = "block";
-  let output = "";
-  results.query.search.forEach((result) => {
-    let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
-    output += `
-    <div class="result p-2">
-    <a href="${resultURL}" target="_blank" rel="noopener" class="h3 fw-bold"
-      >${result.title}</a
-    >
-    <br />
-    <a
-      href="${resultURL}"
-      target="_blank"
-      rel="noopener"
-      class="fs-5 text-success"
-      >${resultURL}</a
-    >
-    <p class="fs-5">
-    ${result.snippet}
-    </p>
-  </div>
-    `;
+    line.style.display = "block";
+    let output = "";
+    results.query.search.forEach((result) => {
+        let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
+        output += `
+            <div class="result p-2">
+                <a href="${resultURL}" target="_blank" rel="noopener" class="h3 fw-bold">${result.title}</a>
+                <br />
+                <a href="${resultURL}" target="_blank" rel="noopener" class="fs-5 text-success">${resultURL}</a>
+                <p class="fs-5">${result.snippet}</p>
+            </div>
+        `;
+    });
     searchResults.innerHTML = output;
-  });
+}
+
+// Clear Results
+function clearResults() {
+    searchResults.innerHTML = "";
+    errorMsg.style.display = "none";
+    line.style.display = "none";
 }
