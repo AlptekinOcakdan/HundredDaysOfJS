8531e1fe2c113614d48655b2ae084b08847a22ca
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-06 13:39:33 +0300

    Image Slider Improved

--- 47ImageSlider/script.js
+++ 47ImageSlider/script.js
@@ -1,56 +1,59 @@
-const slides = document.querySelectorAll(".slide");
-const next = document.querySelector(".next");
-const prev = document.querySelector(".prev");
-const autoScroll = true;
-let slideInterval;
-intervalTime = 5000;
+function setupSlider() {
+    const slides = document.querySelectorAll(".slide");
+    const next = document.querySelector(".next");
+    const prev = document.querySelector(".prev");
+    let shouldAutoScroll = true;
+    const intervalTime = 5000;
+    let slideInterval;
 
-// Next Button
-const nextSlide = () => {
-  const current = document.querySelector(".current");
-  current.classList.remove("current");
-  if (current.nextElementSibling) {
-    current.nextElementSibling.classList.add("current");
-  } else {
-    slides[0].classList.add("current");
-  }
-  current.classList.remove("current");
-};
+    // Next Button
+    const nextSlide = () => {
+        const current = document.querySelector(".current");
+        current.classList.remove("current");
+        if (current.nextElementSibling) {
+            current.nextElementSibling.classList.add("current");
+        } else {
+            slides[0].classList.add("current");
+        }
+        current.classList.remove("current");
+    };
 
-// Prev Button
-const prevSlide = () => {
-  const current = document.querySelector(".current");
-  current.classList.remove("current");
-  if (current.previousElementSibling) {
-    current.previousElementSibling.classList.add("current");
-  } else {
-    slides[slides.length - 1].classList.add("current");
-  }
-  current.classList.remove("current");
-};
+    // Prev Button
+    const prevSlide = () => {
+        const current = document.querySelector(".current");
+        current.classList.remove("current");
+        if (current.previousElementSibling) {
+            current.previousElementSibling.classList.add("current");
+        } else {
+            slides[slides.length - 1].classList.add("current");
+        }
+        current.classList.remove("current");
+    };
 
-// Add EventListeners
-next.addEventListener("click", () => {
-  nextSlide();
-  if (autoScroll) {
-    clearInterval(slideInterval);
-    auto();
-  }
-});
+    // Add EventListeners
+    next.addEventListener("click", () => {
+        clearInterval(slideInterval); // Clear the previous interval
+        nextSlide();
+        if (shouldAutoScroll) {
+            slideInterval = setInterval(nextSlide, intervalTime); // Start a new interval
+        }
+    });
 
-prev.addEventListener("click", () => {
-  prevSlide();
-  if (autoScroll) {
-    clearInterval(slideInterval);
-    auto();
-  }
-});
+    prev.addEventListener("click", () => {
+        clearInterval(slideInterval); // Clear the previous interval
+        prevSlide();
+        if (shouldAutoScroll) {
+            slideInterval = setInterval(nextSlide, intervalTime); // Start a new interval
+        }
+    });
 
-// Auto Scroll
-if (autoScroll) {
-  function auto() {
-    slideInterval = setInterval(nextSlide, intervalTime);
-  }
+    // Auto Scroll
+    if (shouldAutoScroll) {
+        function auto() {
+            slideInterval = setInterval(nextSlide, intervalTime);
+        }
+        auto();
+    }
 }
 
-auto();
+setupSlider();
