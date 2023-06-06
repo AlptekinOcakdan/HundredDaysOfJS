5f3def599b7d5ea903924ab93c9af351db9b8acd
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-06 12:07:22 +0300

    Random Joke Generator App (API changes)

--- 42AsyncAwaitRandomJoke/script.js
+++ 42AsyncAwaitRandomJoke/script.js
@@ -1,6 +1,6 @@
 const joke = document.querySelector(".joke");
 const btn = document.querySelector(".btn");
-const url = "http://api.icndb.com/jokes/random";
+const url = "https://api.chucknorris.io/jokes/random";
 
 btn.addEventListener("click", getJoke);
 
@@ -8,7 +8,7 @@ async function getJoke() {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        joke.innerHTML = data.value.joke;
+        joke.innerHTML = data.value;
     } catch (error) {
         joke.innerHTML = error.message;
     }
